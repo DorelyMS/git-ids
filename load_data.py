1b71477d9fbe89d2cccd60fd801f897436537ba4
maggiemusa <maggie_musa@hotmail.com>
2019-10-03 21:25:36 -0500

    modulo load_data

---
+++ load_data.py
@@ -0,0 +1,14 @@
+
+#Este módulo carga la información del archivo indicado, en este caso del consumo de agua en la Cuidad de México, el cual es un csv separado por punto y comas e imprime el número de observaciones y variables.
+
+import pandas as pd
+
+def carga_archivo(archivo):
+    return pd.read_csv(archivo,sep=';')
+
+
+def observaciones_variables(archivo):
+    print('Número de observaciones:',archivo.shape[0],',', 'Número de variables:',archivo.shape[1],'\n') #numero       de observaciones y variables.
+    
+    
+    
